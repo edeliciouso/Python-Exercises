0ccd54d6e2a6fee942aa86f9ea0488c4373ddfc9
Ede;yne Keisha <edelyne.keisha@binus.ac.id>
2022-09-29 18:07:51 +0700

    find the point exercise

--- exercise5/findpoint.py
+++ exercise5/findpoint.py
@@ -3,7 +3,7 @@ b = int(input("Enter the point of py: "))
 c = int(input("Enter the point of qx: "))
 d = int(input("Enter the point of qy: "))
 
-rx = 2 * (c - a)
-ry = 2 * (d - b)
+rx = (2*c) - a
+ry = (2*d) - b
 
 print("The point of r is: (" + str(rx) + "," + str(ry) + ")")
